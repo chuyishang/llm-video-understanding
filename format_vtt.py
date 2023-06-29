5fe873d74a17416bdcb611ffffa03febfd873e90
chuyishang <chuyishang@berkeley.edu>
2023-06-29 16:35:38 +0000

    processed data for coffee demo

--- format_vtt.py
+++ format_vtt.py
@@ -6,16 +6,26 @@ with open("./COIN/demo_coffee.txt") as f:
     data = data[0].split(",")
     print(data)
 
-dict = {}
-for id in data:
-    entry = {}
-    start, end, text = [], [], []
-    try:
-    for caption in webvtt.read('"/shared/medhini/COIN/asr_vtt/'+id+".en.vtt"):
-        start.append(caption.start)
-        end.append(caption.end)
-        text.append(caption.text)
-    dict[id] = {start, end, text}
+f = open("./COIN/idd_to_vtt.json")
+linker = json.load(f)
+f.close()
 
-with open("./COIN/json_outputs/demo_coffee.json") as outfile:
-    json.dump(dict)
+output = {}
+valid = []
+for id in data:
+    try:
+        start, end, text = [], [], []
+        for caption in webvtt.read(linker[id]):
+            start.append(caption.start)
+            end.append(caption.end)
+            text.append(caption.text)
+        output[id] = {"start": start, "end": end, "text": text}
+        print(linker[id])
+        valid.append(id)
+    except:
+        print("ERROR!", id)
+print(len(output))
+print("VALID", valid)
+
+with open("./demo_coffee.json", "w") as outfile:
+    json.dump(output, outfile)
